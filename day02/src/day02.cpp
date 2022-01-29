d453c2c4aeab68fa19575178bc4935086b93c177
pi <m.komijani@gmail.com>
2022-01-29 13:52:38 +0330

    day02 added

---
+++ day02/src/day02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <random>
+#include <cstdlib>
+
+#include <Eigen/Dense>
+
+#include "open3d/Open3D.h"
+
+int main() {
+    std::cout << "Hello" << std::endl;
+}
